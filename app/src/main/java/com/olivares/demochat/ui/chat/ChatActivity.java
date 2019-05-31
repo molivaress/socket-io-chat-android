815f8f73cb2d7e0e906d8a5daef9a36e5363733c
Mayer Olivares Salinas <mayerolivares_15@hotmail.com>
2019-05-31 16:48:19 -0500

    add icon

--- app/src/main/java/com/olivares/demochat/ui/chat/ChatActivity.java
+++ app/src/main/java/com/olivares/demochat/ui/chat/ChatActivity.java
@@ -1,8 +1,6 @@
 package com.olivares.demochat.ui.chat;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.transition.Explode;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +13,6 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setEnterTransition(new Explode());
-            getWindow().setExitTransition(new Explode());
-        }
-
         setContentView(R.layout.activity_main);
     }
 }
